0dc3d636b07db63d575e2ebbf6989ca7fc6425a9
Pato-Bett <patobett588@gmail.com>
2023-10-04 12:51:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1=31;
+const num2=2;
+let multiply=num1*num2;
+let random=Math.floor(Math.random()*10)
 
-// Write your code here
+let num3=29;
+let num4=5;
+var mod=num3%num4
+
+
+let max=Math.max(1,5,14,20)
